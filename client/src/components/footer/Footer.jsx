41726301b8b133b95170faeee81b3479f1bd7096
DESKTOP-VH4UGLQ\TJ <ygs@mail.com>
2024-01-02 17:36:26 +0900

    design : VisualSlide 완성, footer 완성
    VisualSlide, footer ui 완성

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import styles from './styles.module.css'
+import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <>
-      <footer>
-        footer
+      <footer className={styles.Wrap}>
+        <div className={styles.footerContainer}>
+          <div className={styles.footerLogoWrap}>
+            <span className={styles.footLogo}>
+              <img src="https://cdn.visitkorea.or.kr/resources/images/common/logo_foot.png" alt="한국관광공사" />
+            </span>
+            <ul className={styles.logoListWrap}>
+              <li>
+                <Link to="https://api.visitkorea.or.kr/#/" target="_blank">
+                  <img src="https://korean.visitkorea.or.kr/resources/images/common/logo_foot_api.png?v=20230601001" alt="" />
+                </Link>
+              </li>
+              <li>
+                <Link to="http://www.wa.or.kr/board/list.asp?search=total&SearchString=%B4%EB%C7%D1%B9%CE%B1%B9%20%B1%B8%BC%AE%B1%B8%BC%AE&BoardID=0006" target="_blank">
+                  <img src="https://cdn.visitkorea.or.kr/resources/images/common/logo_foot_wa.png" alt="" />
+                </Link>
+              </li>
+              <li>
+                <Link to="https://kto.visitkorea.or.kr/kor.kto" target="_blank">
+                  <img src="https://cdn.visitkorea.or.kr/resources/images/common/logo_foot_gg.png" alt="" />
+                </Link>
+              </li>
+              <li>
+                <Link to="https://www.mcst.go.kr/kor/main.jsp" target="_blank">
+                  <img src="https://cdn.visitkorea.or.kr/resources/images/common/logo_foot_mg.png" alt="" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </footer>
     </>
   );
--- client/src/components/main/visual/VisualSlide.jsx
+++ client/src/components/main/visual/VisualSlide.jsx
@@ -1,31 +1,32 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import { Controller, Navigation, Pagination, Autoplay} from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Controller, Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation'
 import { Link } from 'react-router-dom';
+import useGetList from '../../../hooks/useGetList';
 
 export default function VisualSlide() {
-  const [firstSwiper,setFirstSwiper] = useState(null);
-  const [secondSwiper,setSecondSwiper] = useState(null);
+  const [list] = useGetList("http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&ServiceKey=CU%2BXIQukCNW8VDOOJDU8QzHHPgrsOso%2FEiDhpWTlD8Lb9q1SYmll5Qp9YK4UsjFNOYVQoLCrMi2s0mfnEPr0iA%3D%3D&listYN=Y&arrange=D&contentTypeId=12&areaCode=39&sigunguCode=&cat1=&cat2=A0101&cat3=&_type=json")
+  const [firstSwiper, setFirstSwiper] = useState(null);
+  const [secondSwiper, setSecondSwiper] = useState(null);
   const [auto, setAuto] = useState(false);
-  const [reset,setReset] = useState(false);
+  const [reset, setReset] = useState(false);
   const [colorNum, setColorNum] = useState(0);
   const visualSwiper = useRef(null)
   const progressFill = useRef(null)
-  const handlePlay = ()=>{
+  const handlePlay = () => {
     visualSwiper.current.swiper.autoplay.resume();
     setAuto(false)
     // setAuto(visualSwiper.current.swiper.autoplay.running)
-    console.log(visualSwiper.current.swiper.autoplay);
   }
-  const handleStop = ()=>{
+  const handleStop = () => {
     visualSwiper.current.swiper.autoplay.pause();
     setAuto(visualSwiper.current.swiper.autoplay.paused)
   }
-  const bgColor = ['rgb(255, 244, 197)','rgb(239, 232, 255)','rgb(232, 235, 240)','rgb(218, 238, 255)','rgb(235, 247, 255)','rgb(235, 245, 250)','rgb(245, 240, 243)','rgb(242, 245, 255)','rgb(255, 230, 229)','rgb(218, 238, 255)','rgb(235, 247, 255)','rgb(242, 235, 255)']
-  const handleColor = (activeIndex)=>{
+  const bgColor = ['rgb(255, 244, 197)', 'rgb(239, 232, 255)', 'rgb(232, 235, 240)', 'rgb(218, 238, 255)', 'rgb(235, 247, 255)', 'rgb(235, 245, 250)', 'rgb(245, 240, 243)', 'rgb(242, 245, 255)', 'rgb(255, 230, 229)', 'rgb(218, 238, 255)', 'rgb(235, 247, 255)', 'rgb(242, 235, 255)']
+  const handleColor = (activeIndex) => {
     setReset(!reset);
     setColorNum(activeIndex)
     /* if(colorNum === 11){
@@ -34,77 +35,64 @@ export default function VisualSlide() {
       setColorNum(colorNum + 1)
     } */
   }
+  useEffect(() => {
+    visualSwiper.current.swiper.autoplay.start();
+    setAuto(false);
+  }, [reset])
   return (
-    <div className={styles.wrap} style={{"background": bgColor[colorNum]}}>
+    <div className={styles.wrap} style={{ "background": bgColor[colorNum] }}>
       <div className={styles.cont}>
         <Swiper
-        ref={visualSwiper}
-        className={styles.leftSwiper}
-        autoplay={{
-          disableOnInteraction: false,
-          delay: 5000
-        }}
-        loop={true}
-        spaceBetween={30}
-        slidesPerView={1}
-        onSwiper={setFirstSwiper}
-        controller={{ control: secondSwiper }}
-        navigation={{
-          prevEl: ".prevBtn",
-          nextEl: ".nextBtn",
-        }}
-        modules={[Controller, Navigation, Pagination, Autoplay]}
-      >
-        <SwiperSlide>
-          <div className={styles.titleWrap}>
-            <em>
-              2023 트렌드 어워드
-            </em>
-            <strong>
-              대한민국구석구석 <br /> 데이터로 본 인기 <br /> 여행지는?
-            </strong>
-            <Link>
-            </Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.titleWrap}>
-            <em>
-              전국 별캉스 추천 숙소 BEST 5
-            </em>
-            <strong>
-              추억에 남을 <br /> 겨울밤 별캉스
-            </strong>
-            <Link>
-            </Link>
-          </div>
-        </SwiperSlide>
+          ref={visualSwiper}
+          className={styles.leftSwiper}
+          autoplay={{
+            disableOnInteraction: false,
+            delay: 5000
+          }}
+          loop={true}
+          spaceBetween={30}
+          slidesPerView={1}
+          onSwiper={setFirstSwiper}
+          controller={{ control: secondSwiper }}
+          navigation={{
+            prevEl: ".prevBtn",
+            nextEl: ".nextBtn",
+          }}
+          modules={[Controller, Navigation, Pagination, Autoplay]}
+        >
+          {list.map((item) => <SwiperSlide>
+            <div className={styles.titleWrap}>
+              <em>
+                2023 트렌드 in 제주
+              </em>
+              <strong>
+                {item.title}
+              </strong>
+              <Link>
+              </Link>
+            </div>
+          </SwiperSlide>)}
         </Swiper>
       </div>
 
       <div className={styles.imgWrap}>
         <Swiper
-        className={styles.rightSwiper}
-        modules={[Controller]}
-        loop={true}
-        spaceBetween={30}
-        slidesPerView={1}
-        onRealIndexChange={realIndex=>{
-          handleColor(realIndex.realIndex)
-        }}
-        onSwiper={setSecondSwiper}
-        controller={{ control: firstSwiper }}
-      >
-        <SwiperSlide>
-          <Link to={"/"}>
-            <img src="https://cdn.visitkorea.or.kr/img/call?cmd=VIEW&id=08383da3-1392-48da-88e7-cc03c090d912&mode=raw" alt="" />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={"/"}>
-            <img src="https://cdn.visitkorea.or.kr/img/call?cmd=VIEW&id=085dc5a3-1836-410c-85d4-7ba18457772e&mode=raw" alt="" />
-          </Link>
-        </SwiperSlide>
+          className={styles.rightSwiper}
+          modules={[Controller]}
+          loop={true}
+          spaceBetween={30}
+          slidesPerView={1}
+          onRealIndexChange={realIndex => {
+            handleColor(realIndex.realIndex)
+          }}
+          onSwiper={setSecondSwiper}
+          controller={{ control: firstSwiper }}
+        >
+          {list.map((item)=><SwiperSlide>
+            <Link to={"/"}>
+              <img src={item.firstimage} alt="" />
+            </Link>
+          </SwiperSlide>)}
         </Swiper>
       </div>
 
@@ -116,7 +104,7 @@ export default function VisualSlide() {
             </span>
           </div>
           <div className={`${styles.pagination} pagination`}>
-            <span className={styles.currentPage}>{colorNum+1}</span>
+            <span className={styles.currentPage}>{colorNum + 1}</span>
             /
             <span className={styles.totalPage}>12</span>
           </div>
@@ -124,8 +112,8 @@ export default function VisualSlide() {
             <button className={`${styles.prevBtn} prevBtn`}></button>
             <button className={`${styles.nextBtn} nextBtn`}></button>
             <div className={styles.btnAuto}>
-              <button className={`${styles.autoPlay} ${!auto ?  styles.auto:''}`} onClick={()=>handlePlay()}></button>
-              <button className={`${styles.autoStop} ${auto ?  styles.auto:''}`} onClick={()=>handleStop()}></button>
+              <button className={`${styles.autoPlay} ${!auto ? styles.auto : ''}`} onClick={() => handlePlay()}></button>
+              <button className={`${styles.autoStop} ${auto ? styles.auto : ''}`} onClick={() => handleStop()}></button>
             </div>
           </div>
         </div>
