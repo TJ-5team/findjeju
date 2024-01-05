cf4e8e67f02ab44025efea5f6c58f1f1eba7cd14
JT <qmfntmchl123@gmail.com>
2024-01-05 17:33:56 +0900

    docs: 회원가입 페이지 수정중 이메일 완료
    * 주소 value 체크 해야함
    * 파일업로드 진행중
    * 약관동의 유효성 체크해야함
    * 폼 유효성 체크 해야함

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
     ]
   }
 
-])
+]);
 
 function App() {
   return (
--- client/src/components/member/join/Join.jsx
+++ client/src/components/member/join/Join.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import ImageUpload from '../../imageUpload/ImageUpload';
 import axios from 'axios';
+import DaumPostcode from 'react-daum-postcode';
 
 export default function Join() {
 
@@ -12,6 +13,10 @@ export default function Join() {
     const [time, setTime] = useState(180);
     const [isActive, setIsActive] = useState(false);
     const [number, setNumber] = useState(0);
+    const [postal, setPostal] = useState(false);
+    const [address, setAddress] = useState('');
+    const [zoneCode, setZoneCode] = useState('');
+
     let pattern_num = /[0-9]/;	// 숫자 
     let pattern_eng = /[a-zA-Z]/;	// 문자 
     let pattern_spc = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
@@ -26,10 +31,12 @@ export default function Join() {
     */
     const pattern_mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const pattern_emoji = /[\uD800-\uDBFF][\uDC00-\uDFFF]/; // 이모지체크
+    const pattern_phone = /^(01[016789]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
 
-    const [check, setCheck] = useState({ name: '', id: '', pass: '', passcheck: '', nickname: '', email: '', echeck: '', eSelf: '', confirm: '' });
-    const [focus, setFocus] = useState({ name: '', id: '', pass: '', passcheck: '', nickname: '', email: '', echeck: '', eSelf: '', confirm: '' });
-    const [validation, setValidation] = useState({ name: '필수 입력 항목입니다.', id: '필수 입력 항목입니다.', pass: '필수 입력 항목입니다.', passcheck: '필수 입력 항목입니다.', nickname: '필수 입력 항목입니다.', email: '이메일 형식에 맞지 않습니다.', echeck: '필수 입력 항목입니다.', eSelf: '필수 입력 항목입니다.', confirm: '' });
+    const [mailCheck, setMailCheck] = useState(false);
+    const [check, setCheck] = useState({ name: '', id: '', pass: '', passcheck: '', nickname: '', email: '', echeck: '', eSelf: '', confirm: '', phone1: '' });
+    const [focus, setFocus] = useState({ name: '', id: '', pass: '', passcheck: '', nickname: '', email: '', echeck: '', eSelf: '', confirm: '', phone1: '' });
+    const [validation, setValidation] = useState({ name: '필수 입력 항목입니다.', id: '필수 입력 항목입니다.', pass: '필수 입력 항목입니다.', passcheck: '필수 입력 항목입니다.', nickname: '필수 입력 항목입니다.', email: '이메일 형식에 맞지 않습니다.', echeck: '필수 입력 항목입니다.', eSelf: '필수 입력 항목입니다.', confirm: '', phone: '필수 입력 항목입니다.' });
 
     const fnSubmit = (e) => {
 
@@ -38,6 +45,8 @@ export default function Join() {
     }
 
     useEffect(() => {
+
+
 
         axios({
 
@@ -225,11 +234,6 @@ export default function Join() {
 
         /* 핸드폰번호 유효성 검사 */
 
-        if (name === 'phone' && value) {
-
-
-
-        }
 
         /* 이용약관 유효성 검사*/
 
@@ -257,6 +261,20 @@ export default function Join() {
 
     }, [form.echeck, form.email, form.eSelf])
 
+    useEffect(() => {
+
+
+        const phoneTotal = `${form.phone1}-${form.phone2}-${form.phone3}`;
+
+        if (!pattern_phone.test(phoneTotal)) {
+            setValidation((validation) => ({ ...validation, phone: '번호 형식에 맞지 않습니다.' }));
+        } else if (pattern_phone.test(phoneTotal)) {
+            setCheck((check) => ({ ...check, phone: 'phone' }));
+            setValidation((validation) => ({ ...validation, phone: '사용 가능한 휴대폰번호입니다.' }));
+        }
+
+    }, [form.phone1, form.phone2, form.phone3])
+
     const getImage = (e, d) => {
         setImage(e);
     }
@@ -275,6 +293,26 @@ export default function Join() {
 
     }
 
+    const completeHandler = (data) => {
+
+        let fullAddress = data.address;
+        let extraAddress = '';
+
+        if (data.addressType === 'R') {
+            if (data.bname !== '') {
+                extraAddress += data.bname;
+            }
+            if (data.buildingName !== '') {
+                extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+            }
+            fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        }
+
+        setAddress(fullAddress);
+        setZoneCode(data.zonecode);
+
+    }
+
     /* const startCountdown = () => {
 
         setIsActive(true);
@@ -288,11 +326,12 @@ export default function Join() {
 
     const handleMail = () => {
 
+        setTime(180);
+
         axios.post("http://127.0.0.1:8000/join/email", { email: form.email, echeck: form.echeck })
             .then((result) => {
 
                 setNumber(result.data.number);
-                console.log(result.data.number);
                 resetCountdown();
                 setIsActive(true);
 
@@ -302,11 +341,23 @@ export default function Join() {
 
     }
 
+
     const handleMailCheck = (e) => {
 
-
+        setMailCheck(true);
 
     }
+
+    useEffect(() => {
+
+        if (mailCheck) {
+            setValidation((validation) => ({ ...validation, email: '이메일 인증이 완료되었습니다.' }));
+        }
+
+    }, [mailCheck]) // 이메일인증 확인버튼
+
+
+
 
     return (
         <>
@@ -365,7 +416,7 @@ export default function Join() {
                             <li className={styles.joinLi}>
                                 <label id="email">* 이메일</label>
                                 <div className={styles.emailWrap}>
-                                    <input type="text" name="email" id="email" className={styles.emailInput} value={form.email} onChange={fnChange} placeholder='이메일' onFocus={handleFocus} />
+                                    <input type="text" name="email" id="email" className={styles.emailInput} value={form.email} onChange={fnChange} placeholder='이메일' onFocus={handleFocus} disabled={mailCheck} />
                                     <span>@</span>
                                     {form.echeck === 'manual'
                                         ?
@@ -377,7 +428,7 @@ export default function Join() {
                                         </div>
                                         :
                                         <select name="echeck" id="echeck" value={form.echeck}  // select의 value를 state와 연동
-                                            onChange={fnChange} onFocus={handleFocus}>
+                                            onChange={fnChange} onFocus={handleFocus} disabled={mailCheck}>
                                             <option value="" disabled>선택해주세요.</option>
                                             <option value="naver.com">naver.com</option>
                                             <option value="hanmail.net">hanmail.com</option>
@@ -393,7 +444,7 @@ export default function Join() {
                                 {(focus.email === 'ok' || focus.echeck === 'manual') && check.email === 'email' || check.echeck === 'echeck'
                                     ? validation.email === '등록된 이메일입니다.' || validation.email === '이메일 형식에 맞지 않습니다.'
                                         ? <span>{validation.email}</span>
-                                        : validation.email === '필수 입력 항목입니다.' ? <span>{validation.email}</span> : <span className={styles.success}>{validation.email}</span>
+                                        : validation.email === '이메일을 인증해주세요.' ? <span>{validation.email}</span> : <span className={styles.success}>{validation.email}</span>
                                     : null
                                 }
                                 <button type='button' className={styles.emailBtn} disabled={(validation.email === '이메일 형식에 맞지 않습니다.' || validation.email === '등록된 이메일입니다.') || emailToggle === true} onClick={() => {
@@ -402,40 +453,46 @@ export default function Join() {
                                 }}>
                                     이메일 인증하기
                                 </button>
-                                {emailToggle ?
+                                {emailToggle && !mailCheck ?
                                     <div className={styles.emailCodeBox}>
                                         <div className={styles.countDown}>{Math.floor(time / 60)}:{time % 60 > 9 ? time % 60 : "0" + time % 60}</div>
                                         <p className={styles.desCode}>이메일로 전송된 인증코드를 입력해주세요.</p>
                                         <div>
                                             <input type="text" name='confirm' id="confirm" value={form.confirm} maxLength={6} placeholder='인증코드 6자리 입력' onChange={fnChange} onFocus={handleFocus} />
-                                            <button type="button" onClick={handleMailCheck} disabled={time === 0 || parseInt(form.confirm) !== number}>확인</button>
+                                            <button type="button" onClick={handleMailCheck} disabled={time === 0 || parseInt(form.confirm) !== number || mailCheck === true}>확인</button>
                                         </div>
                                         {validation.confirm === '올바른 코드가 아닙니다.' ? <span className={styles.check}>{validation.confirm}</span> : null}
-                                        <p>이메일을 받지 못하셨나요? <span>이메일 재전송하기</span></p>
+                                        <p>이메일을 받지 못하셨나요? <span onClick={handleMail}>이메일 재전송하기</span></p>
                                     </div> : null}
                             </li>
                             <li className={styles.joinLi}>
                                 <label id="phone">* 핸드폰번호</label>
                                 <div className={styles.phoneWrap}>
-                                    <input type="text" name="phone1" id="phone" value={form.phone1} onChange={fnChange} placeholder='010' />
+                                    <input type="text" name="phone1" id="phone" value={form.phone1} onChange={fnChange} placeholder='010' onFocus={handleFocus} maxLength={3} />
                                     <span>-</span>
-                                    <input type="text" name="phone2" id="phone" value={form.phone2} onChange={fnChange} placeholder='1234' />
+                                    <input type="text" name="phone2" id="phone" value={form.phone2} onChange={fnChange} placeholder='1234' maxLength={4} />
                                     <span>-</span>
-                                    <input type="text" name="phone3" id="phone" value={form.phone3} onChange={fnChange} placeholder='5678' />
+                                    <input type="text" name="phone3" id="phone" value={form.phone3} onChange={fnChange} placeholder='5678' maxLength={4} />
                                 </div>
+                                {check.phone1 === 'phone' || focus.phone1 === 'ok' ? validation.phone === '번호 형식에 맞지 않습니다.' ? <span> {validation.phone}</span> : <span className={styles.success}> {validation.phone}</span> : null}
                             </li>
                             <li className={styles.joinLi}>
                                 <label id="postal">* 우편번호</label>
                                 <div className={styles.postalWrap}>
-                                    <input type="text" name="postal" id="postal" value={form.postal} onChange={fnChange} placeholder='우편번호 입력' />
-                                    <button className={styles.postalBtn}>우편번호 검색</button>
+                                    <input type="text" name="postal" id="postal" value={zoneCode} onChange={fnChange} placeholder='우편번호 입력' />
+                                    <button type="button" className={styles.postalBtn} onClick={() => {
+
+                                        setPostal((postal) => !postal);
+
+                                    }}>{postal ? 'X' : '우편번호 검색'}</button>
                                 </div>
+                                {postal ? <DaumPostcode onComplete={completeHandler} /> : null}
                             </li>
                             <li className={styles.joinLi}>
                                 <label id="address">* 주소</label>
                                 <div className={styles.addressWrap}>
-                                    <input type="text" name="address" id="address" value={form.address} onChange={fnChange} />
-                                    <input type="text" name="address" id="address" value={form.address} onChange={fnChange} />
+                                    <input type="text" name="address" id="address" value={address} />
+                                    <input type="text" name="address" id="address" value={form.address} onChange={fnChange} placeholder='상세주소입력' />
                                 </div>
                             </li>
                             <li className={styles.joinLi}>
@@ -446,9 +503,9 @@ export default function Join() {
                             <li className={styles.joinLi}>
                                 <label id="terms">* 약관동의</label>
                                 <div className={styles.terms}>
-                                    <p><input type="checkbox" name="terms" /><span>개인정보 수집이용 동의 (필수)</span></p>
-                                    <p><input type="checkbox" name="terms" /><span>전자금융거래 이용약관 동의 (필수)</span></p>
-                                    <p><input type="checkbox" name="terms" /><span>마케팅 정보 메일, SMS 수신동의 (선택)</span></p>
+                                    <p><input type="checkbox" name="terms1" value={form.terms1} onChange={fnChange} /><span>개인정보 수집이용 동의 (필수)</span></p>
+                                    <p><input type="checkbox" name="terms2" value={form.terms2} onChange={fnChange} /><span>전자금융거래 이용약관 동의 (필수)</span></p>
+                                    <p><input type="checkbox" name="terms3" value={form.terms3} onChange={fnChange} /><span>마케팅 정보 메일, SMS 수신동의 (선택)</span></p>
                                 </div>
                             </li>
                         </ul>
