dd7f6363686b1642a3df43d1a3499b36b0f912ce
HN <iamonlyone.u1@gmail.com>
2024-01-17 12:01:32 +0900

    design: 상세페이지 무장애여행 DetailList 컴포넌트 활용하여 UI 수정
    DetailComfortable 컴포넌트의 list들을 DetailList 컴포넌트로 활용
    사용안하는 DetailPageCultural 페이지 삭제

--- client/src/components/DetailPage/categorypage/DetailComfortable.jsx
+++ client/src/components/DetailPage/categorypage/DetailComfortable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "../../DetailPage/styles.module.css";
+import DetailList from './DetailList';
 
 export default function DetailComfortable({ contentid, comfortableInfo }) {
   // const [comfortable] = useGetList(`http://apis.data.go.kr/B551011/KorWithService1/detailWithTour1?serviceKey=nyjoBggUlH0et5JY2fC9TW7%2BuSsx%2BIGHKWsgAuOWswMCtns64Y3M1Z%2BGROfg6L5ONigYQx6N%2BmqDCpABn3PmeQ%3D%3D&contentId=${contentid}&MobileOS=ETC&MobileApp=AppTest&_type=json`);
@@ -8,7 +9,20 @@ export default function DetailComfortable({ contentid, comfortableInfo }) {
     <>
       {comfortableInfo && comfortableInfo.map(info =>
         <ul className={styles.detailInfo} key={contentid}>
-          <li>
+          <DetailList name={"문의 및 안내"} list={info.parking} />
+          <DetailList name={"접근로"} list={info.publictransport} />
+          <DetailList name={"휠체어"} list={info.wheelchair} />
+          <DetailList name={"출입통로"} list={info.exit} />
+          <DetailList name={"엘리베이터"} list={info.elevator} />
+          <DetailList name={"화장실"} list={info.restroom} />
+          <DetailList name={"객실"} list={info.room} />
+          <DetailList name={"점자블록"} list={info.braileblock} />
+          <DetailList name={"보조견동반"} list={info.helpdog} />
+          <DetailList name={"안내요원"} list={info.guidehuman} />
+          <DetailList name={"점자홍보물 및 점자표지판"} list={info.brailepromotion} />
+          <DetailList name={"유모차"} list={info.stroller} />
+          <DetailList name={"수유실"} list={info.lactationroom} />
+          {/* <li>
             <strong>장애인 주차 안내</strong>
             <span>{info.parking}</span>
           </li>
@@ -59,7 +73,7 @@ export default function DetailComfortable({ contentid, comfortableInfo }) {
           <li>
             <strong>수유실</strong>
             <span>{info.lactationroom}</span>
-          </li>
+          </li> */}
         </ul>
       )}
     </>
